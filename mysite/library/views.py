40002432e8ee8dcf4e28541105be17daec2d2b93
andrejt <andrej.t23@yandex.ru>
2022-03-11 11:32:52 +0300

    сделал основы шаблонов

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,22 +1,17 @@
-from django import HttpResponse
-from django import loader
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+def book(request):
+    return render(request, 'library/index2.html')
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+def Genres(request):
+    return render(request, 'library/autors.html')
+
+def Authors(request):
+    return render(request, 'library/Genres.html')
