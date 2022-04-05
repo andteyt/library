e9e610f9fd031d560b25dd5373eb117fcd02647d
andrejt <andrej.t23@yandex.ru>
2022-04-05 12:09:14 +0300

    соединяю файлы своих работ и алины

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -3,15 +3,25 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
-def book(request):
-    return render(request, 'library/book.html')
+def index(request):
+    author = Authors.objects.all()  #select_related(id)
+    book = Books.objects.all()
+    context = {'author': author, 'books': book}
+    return render(request, 'library/index.html', context)
 
 def Genres(request):
-    return render(request, 'library/autors.html')
+    Genres = Genres.objects.select_related(id)
+    context = {'Genres': Genres}
+    return render(request, 'library/Genres.html', context)
 
-def Authors(request):
-    return render(request, 'library/Genres.html')
+def author(request,author_id):
+    author = Authors.objects.get(pk=author_id)
+    books = Books.objects.filter(authors=author_id)
+    context = {'author': author, 'books': books}
+    return render(request, 'library/author.html', context)
+
+def books(request,book_id):
+    book = Books.objects.get(pk=book_id)
+    context = {'book': book}
+    return render(request, 'library/book.html', context)
+
