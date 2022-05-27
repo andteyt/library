df5c1a070106f6256e30efbdc0ff43352b30a3fe
vrsamsung <90859806+vrsamsung@users.noreply.github.com>
2022-05-27 11:57:24 +0300

    добавила картинку к автору, сталось завершить базу данных и HTML

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -6,8 +6,8 @@ class Authors(models.Model):
     name = models.CharField(max_length=15)
     surname = models.CharField(max_length=30)
     patronymic = models.CharField(max_length=20)
-    img = models.ImageField(upload_to='img/author',
-                      height_field=100, width_field=100)
+    img = models.ImageField(upload_to='img/author')
+
 class Genres(models.Model):
     title = models.CharField(max_length=30)
 
@@ -16,6 +16,6 @@ class Books(models.Model):
     title = models.CharField(max_length=100)
     year = models.IntegerField(max_length=4)
     publication_house = models.CharField(max_length=15)
-class Books_Genres(models.Model):
-    b_g_books = models.ForeignKey(Books, on_delete=models.CASCADE)
-    b_g_genres = models.ForeignKey(Genres, on_delete=models.CASCADE)
+    Genres = models.ManyToManyField(Genres)
+
+
