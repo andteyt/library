06ac1864c51172fd5666e46780ef18511bf11a46
blue_lapot <97691074+blue-lapot@users.noreply.github.com>
2022-06-03 11:04:40 +0300

    .

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+
 class Authors(models.Model):
-    biography = models.CharField(max_length=5000)
+    biography = models.TextField()
     name = models.CharField(max_length=15)
     surname = models.CharField(max_length=30)
     patronymic = models.CharField(max_length=20)
+    img = models.ImageField(upload_to='img/author')
 
 class Genres(models.Model):
     title = models.CharField(max_length=30)
@@ -15,6 +19,4 @@ class Books(models.Model):
     title = models.CharField(max_length=100)
     year = models.IntegerField(max_length=4)
     publication_house = models.CharField(max_length=15)
-class Books_Genres(models.Model):
-    b_g_books = models.ForeignKey(Books, on_delete=models.CASCADE)
-    b_g_genres = models.ForeignKey(Genres, on_delete=models.CASCADE)
+    Genres = models.ManyToManyField(Genres)
